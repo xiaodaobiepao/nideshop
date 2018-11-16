30b908bf0c7bf3aaae30e9e62ceada82704422ce
xiaodaobiepao <1310851368@qq.com>
2018-11-16 09:02:36 +0000

    xiugaipeizhi

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
   database: 'nideshop',
   prefix: 'nideshop_',
   encoding: 'utf8mb4',
-  host: '127.0.0.1',
-  port: '3306',
-  user: 'root',
-  password: 'root',
+  host: 'process.env.MYSQL_HOST',
+  port: 'process.env.MYSQL_PORT',
+  user: 'process.env.ACCESSKEY',
+  password: 'process.envSECRETKEY',
   dateStrings: true
 };
